f2397b00b08a18238630593c171daa60a970db49
Adam Biegert <theadman2092@ymail.com>
2025-11-22 12:31:03 -0700

    update(stark_frontdesk): Update Server Side, Config, & FXManifest

---
+++ fxmanifest.lua
@@ -0,0 +1,41 @@
+fx_version 'cerulean'
+
+use_experimental_fxv2_oal 'yes'
+
+lua54 'yes'
+
+game 'gta5'
+
+name 'stark_frontdesk'
+
+author 'Adama Stark'
+
+version '1.0.0'
+
+repository 'https://github.com/Stark-Development-Studios/stark_frontdesk'
+
+description 'A Civilian LEO/EMS Non-Emergency Interaction Script For Qbox & QBCore'
+
+ox_lib 'locale'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+}
+
+client_scripts {
+    'bridge/client/**.lua',
+    'client/*.lua',
+}
+
+server_scripts {
+    'server/*.lua'
+}
+
+files {
+    'shared/config.lua',
+    'locales/*.json',
+}
+
+dependency {
+    'ox_lib'
+}
---
+++ server/main.lua
@@ -0,0 +1,7 @@
+if not lib.checkDependency('ox_lib', '3.32.0', true) then return end
+
+local Config = require 'shared.config'
+
+if Config.VersionCheck then
+    lib.versionCheck('Stark-Development-Studios/stark_frontdesk')
+end
