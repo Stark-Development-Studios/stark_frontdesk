d0a2c295059e5fb1ef34f526ed117a59b62e85f8
Adam Biegert <theadman2092@ymail.com>
2025-11-22 13:01:35 -0700

    update(stark_frontdesk): Update Config

--- shared/config.lua
+++ shared/config.lua
@@ -1,3 +1,61 @@
 return {
     VersionCheck = true,
+
+    Framework = 'qbx', -- supported: 'qb' or 'qbx'
+
+    Target = 'ox',     -- supported: 'qb' or 'ox'
+
+    Notify = {
+        enabled = true,   -- True To Enable Notifications, False To Disable
+        style = 'ox',     --  supported: 'qb', 'ox', or 'lation'
+        duration = 10000, -- Configurable Notification Duration Length
+    },
+
+    Dispatch = {
+        enabled = false,       -- True To Enable A Dispatch System, False To Disable
+        style = 'cd_dispatch', -- supported: 'cd_dispatch' or 'ps-dispatch'
+    },
+
+    Menu = 'ox', -- supported: 'qb', 'ox', or 'lation'
+
+    PoliceLocations = {
+        [1] = {
+            name = 'Mission Row Police Department',
+            coords = '',
+        },
+
+        [2] = {
+            name = 'Los Santos County Sheriff\'s Office',
+            coords = '',
+        },
+
+        [3] = {
+            name = 'San Anderas State Police',
+            coords = '',
+        }
+    },
+
+    AmbulanceLocations = {
+        [1] = {
+            name = 'Pillbox Hill Medical Center',
+            coords = '',
+        },
+
+        [2] = {
+            name = 'Mount Zonah Medical Center',
+            coords = '',
+        },
+
+        [3] = {
+            name = 'Sandy Shores Clinic',
+            coords = '',
+        }
+    },
+
+    MechanicLocations = {
+        [1] = {
+            name = 'Cruisin\' Craftsmen',
+            coords = '',
+        },
+    },
 }
